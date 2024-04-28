6c07a2489e89031747f4e5b34b4b2340bc6d1550
REValdiviezo <ezequielrvaldiviezo@gmail.com>
2024-04-28 03:33:04 -0300

    Primer commit Todo-TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-6xl font-bold text-blue-300 border-2 border-red-600'>
+      Hola Mundo
+    </div>
+  )
+}
+
+export default App
